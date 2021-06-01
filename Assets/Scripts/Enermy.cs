846cf9ee39ece047555d2feab910051956a87142
dudrhkd2569 <80015342+dudrhkd2569@users.noreply.github.com>
2021-06-01 11:48:22 +0900

    총쏘기

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -17,10 +17,20 @@ public class Enermy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(target.position, transform.position)<10f)
+        if (Vector3.Distance(target.position, transform.position) < 10f)
         {
             Vector3 trans = target.position - transform.position;
             rig.AddForce(trans * speed * Time.deltaTime);
+        }
+    }
+    private bool scored = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && scored == false)
+        {
+            scored = true; GameManager.instance.AddScore(1);
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
+            scored = false;
         }
     }
 }
